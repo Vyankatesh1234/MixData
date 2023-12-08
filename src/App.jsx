7ad4515e0bb7599914b5f356561a1e6438a3cebd
Vyankatesh1234 <vyankateshkawarase@opticalarc.com>
2023-12-08 11:52:08 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { SortableTable } from './Components/AdminData'
-// import { MembersTable } from './Components/dumy'
-// import { MembersTable } from "./Components/dummy";
 
 export default function App() {
   return (
     <div>
       <SortableTable/>
-      {/* <MembersTable/> */}
-      {/* <MembersTable/> */}
     </div>
   )
 }
