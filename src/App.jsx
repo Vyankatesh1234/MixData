76371de0e57163712e9867a3d7f58ebb4cbe3144
Vyankatesh1234 <vyankateshkawarase@opticalarc.com>
2023-12-08 11:46:47 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { SortableTable } from './Components/AdminData'
+// import { MembersTable } from './Components/dumy'
+// import { MembersTable } from "./Components/dummy";
+
+export default function App() {
+  return (
+    <div>
+      <SortableTable/>
+      {/* <MembersTable/> */}
+      {/* <MembersTable/> */}
+    </div>
+  )
+}
---
+++ src/Components/AdminData.jsx
@@ -0,0 +1,198 @@
+import { useEffect, useState } from 'react';
+import {
+  Card,
+  Typography,
+  Button,
+  CardBody,
+  CardFooter,
+  IconButton,
+  Tooltip
+} from "@material-tailwind/react";
+import { PencilIcon } from "@heroicons/react/24/solid";
+
+export function SortableTable() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [tableData, setTableData] = useState([]);
+  const itemsPerPage = 5;
+
+  async function fetchData() {
+    try {
+      const response = await fetch('members.json');
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      setTableData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [currentPage]);
+
+  const totalPages = Math.ceil(tableData.length / itemsPerPage);
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  return (
+    <Card className="h-full w-full">
+      <CardBody className="overflow-scroll px-0">
+        <table className="mt-4 w-full min-w-max table-auto text-left">
+          <thead>
+            <tr>
+              <th className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  Member
+                </Typography>
+              </th>
+              <th className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  Function
+                </Typography>
+              </th>
+              <th className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  EMP_ID
+                </Typography>
+              </th>
+              <th className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  Employed
+                </Typography>
+              </th>
+              <th className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  Edit
+                </Typography>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+          {tableData
+    .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+    .map(({ name, email, job, org, empid, date }, index) => (
+      <tr key={index}>
+                <td className="p-4">
+                  <div className="flex items-center gap-3">
+                    <div className="flex flex-col">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {name}
+                      </Typography>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal opacity-70"
+                      >
+                        {email}
+                      </Typography>
+                    </div>
+                  </div>
+                </td>
+                <td className="p-4">
+                  <div className="flex flex-col">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {job}
+                    </Typography>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal opacity-70"
+                    >
+                      {org}
+                    </Typography>
+                  </div>
+                </td>
+                <td className="p-4">
+                  <div className="flex flex-col">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {empid}
+                    </Typography>
+                  </div>
+                </td>
+                <td className="p-4">
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                  >
+                    {date}
+                  </Typography>
+                </td>
+                <td className="p-4">
+                  <Tooltip content="Edit User">
+                    <IconButton variant="text">
+                      <PencilIcon className="h-4 w-4" />
+                    </IconButton>
+                  </Tooltip>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </CardBody>
+      <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+        <Typography variant="small" color="blue-gray" className="font-normal">
+          Page {currentPage} of {totalPages}
+        </Typography>
+        <div className="flex gap-2">
+          <Button
+            variant="outlined"
+            size="sm"
+            onClick={handlePreviousPage}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outlined"
+            size="sm"
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </Button>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
